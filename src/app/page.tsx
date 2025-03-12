4545bcbb42c3c748f1a98c5eee89e95b85aef5eb
LUIS MIGUEL DAVID CAMPO <134025037+LUISDACA@users.noreply.github.com>
2025-03-12 08:00:33 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex flex-center justify-center w-screen h-screen bg-cyan-400">
+    <main className="flex items-center justify-center w-screen h-screen bg-cyan-400">
       <h1 className="text-cyan-700">Luis Miguel David Campo</h1>
     </main>
   );
-}      
+}  

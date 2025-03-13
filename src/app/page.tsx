644ef0f77ef07d57d3a4cbf047497890014b90aa
LUISDACA <luis.davidca@campusucc.edu.co>
2025-03-13 07:12:26 -0500

    Cambio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex items-center justify-center w-screen h-screen bg-cyan-400">
-      <h1 className="text-cyan-700">Luis Miguel David Campo</h1>
+    <main className="flex items-center justify-center w-screen h-screen bg-red-400">
+      <h1 className="text-red-700">Luis Miguel David Campo</h1>
     </main>
   );
 }  
